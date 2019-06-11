1d49a074cb44de39624c15e6c2a95ecf1f7feddd
brittanyrw <breneewalker@gmail.com>
2019-06-10 22:36:36 -0400

    initial commit, added in initial data and set up component structure

---
+++ src/components/Groups.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <li v-for="group in groups" v-bind:key="group.id">
+    {{ group.name }}
+    </li>
+  </div>
+</template>
+
+<script>
+
+import {groupList} from "../data/groups";
+
+export default {
+  name: 'Groups',
+  data: function () {
+		return {
+			groups: groupList,
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>K-pop Tracker</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse enim nisi officia unde. Non aliquid, perferendis explicabo cum, architecto eius temporibus maiores soluta velit sed debitis aliquam aut est?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
